0879f2b132e81ac9cc95b2065a1ed024e7a4a7a6
kangjian <kangjian@360.cn>
2023-05-11 00:52:03 +0800

    update

--- templates/routes/other.js
+++ templates/routes/other.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/map', function(req, res, next) {
   res.render('other/map', { title: 'Express' });
+});
+router.get('/readme', function(req, res, next) {
+  res.render('other/readme', { title: 'Express' });
 });
 
 module.exports = router;
