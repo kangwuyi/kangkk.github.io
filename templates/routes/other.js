44bda03c2ede456210be0fbdaa615e2197144224
kangjian <kangjian@360.cn>
2023-05-13 01:42:43 +0800

    修改简介

--- templates/routes/other.js
+++ templates/routes/other.js
@@ -2,10 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/map', function(req, res, next) {
-  res.render('other/map', { title: 'Express' });
+  res.render('other/map');
 });
 router.get('/readme', function(req, res, next) {
-  res.render('other/readme', { title: 'Express' });
+  res.render('other/readme');
 });
 
 module.exports = router;
