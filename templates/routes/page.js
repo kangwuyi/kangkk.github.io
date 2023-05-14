0a7f970704388f2234023444e5c1abf5f4a1d00b
kangjian <kangjian@360.cn>
2023-05-14 17:55:01 +0800

    修改模版

--- templates/routes/page.js
+++ templates/routes/page.js
@@ -1,9 +1,41 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/index', function(req, res, next) {
+  res.render('page/index', {
+    kcFileId: '123',
+    kcFileName: 'abc',
+    kcFileAddr: '1234',
+    description: 'description',
+    keyWorlds: 'keyWorlds'
+  });
 });
+router.get('/header', function(req, res, next) {
+  res.render('page/header', {
+    kcFileName: 'title',
+    description: 'description',
+    keyWorlds: 'keyWorlds'
+  });
+});
+router.get('/footer', function(req, res, next) {
+  res.render('page/footer', {
+    kcFileId: '123',
+    kcFileName: 'abc',
+    kcFileAddr: '1234',
+  });
+});
+// //
+// router.get('/candies', function(req, res, next) {
+//   res.render('page/candies', { title: 'Express' });
+// });
+// router.get('/chestnut', function(req, res, next) {
+//   res.render('page/chestnut', { title: 'Express' });
+// });
+// router.get('/longan', function(req, res, next) {
+//   res.render('page/longan', { title: 'Express' });
+// });
+// router.get('/waterChestnut', function(req, res, next) {
+//   res.render('page/waterChestnut', { title: 'Express' });
+// });
 
 module.exports = router;
