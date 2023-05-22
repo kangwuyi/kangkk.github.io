901ddcd9fed9df5b10abb410bec9a2e7a913de43
kangjian <kangjian@360.cn>
2023-05-22 18:39:55 +0800

    update

--- templates/routes/index.js
+++ templates/routes/index.js
@@ -3,7 +3,31 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    list: [
+      // 'home/header',
+      // 'home/footer',
+      'home/left',
+      'home/right',
+      'home/index',
+      //
+      'menu/left',
+      'menu/right',
+      'menu/index',
+      //
+      'other/map',
+      'other/readme',
+      //
+      'page/header',
+      'page/footer',
+      'page/index',
+      'page/candies',
+      'page/chestnut',
+      'page/longan',
+      'page/candies',
+      'page/waterChestnut',
+    ]
+  });
 });
 
 module.exports = router;
