4493482cceb13b960684b6eb1fa541d2963e7d19
kwy <365@kangwuyi.com>
2025-03-23 11:30:35 +0000

    del:file

--- app/fs.js
+++
@@ -1,8 +0,0 @@
-const _ = require('underscore');
-
-function cheackExtnameIsMd(extname) {
-  return _.indexOf(['.md', '.markdown', '.MARKDOWN', '.MD'], extname) !== -1
-}
-module.exports = {
-  cheackExtnameIsMd
-}
